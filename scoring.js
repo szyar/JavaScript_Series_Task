89ea39da3da9bdacbab5394826da796d2e498d60
szyar <soezayarszyar@gmail.com>
2020-07-22 12:10:05 +0630

    finishing javascript coding

---
+++ scoring.js
@@ -0,0 +1,81 @@
+$(function () {
+    function score_indicate() {
+        let subject_points = [
+            Number($("#national_language").val()),
+            Number($("#english").val()),
+            Number($("#mathematics").val()),
+            Number($("#science").val()),
+            Number($("#society").val()),
+        ];
+
+        let sum = subject_points.reduce((all, item) => all + item);
+
+        $("#sum_indicate").text(sum);
+        $("#average_indicate").text(sum / subject_points.length);
+    }
+
+    function get_achievement() {
+        let averageIndicate = $("#average_indicate").text();
+
+        if (averageIndicate >= 80) {
+            return "A";
+        } else if (averageIndicate >= 60) {
+            return "B";
+        } else if (averageIndicate >= 40) {
+            return "C";
+        } else {
+            return "D";
+        }
+    }
+
+    function get_pass_or_failure() {
+        let subject_points = [
+            Number($("#national_language").val()),
+            Number($("#english").val()),
+            Number($("#mathematics").val()),
+            Number($("#science").val()),
+            Number($("#society").val()),
+        ];
+
+        let number = subject_points.length;
+
+        let judge = "Pass";
+
+        for (let i = 0; i < number; i++) {
+            if (subject_points[i] < 60) {
+                judge = "Fail";
+            }
+        }
+
+        return judge;
+    }
+
+    function judgment() {
+        let achievement = get_achievement();
+        let pass_or_failure = get_pass_or_failure();
+
+        $("#declaration").empty();
+        $("#declaration").append(
+            `<label id="alert-indicate" class="alert alert-info">Your grade is ${achievement} and ${pass_or_failure}</label>`
+        );
+
+    }
+
+    $("#national_language, #english, #mathematics, #science, #society").change(
+        function () {
+            score_indicate();
+        }
+    );
+
+    $("#btn-evaluation").click(function () {
+        $("#evaluation").text(get_achievement());
+    });
+
+    $("#btn-judge").click(function () {
+        $("#judge").text(get_pass_or_failure());
+    });
+
+    $("#btn-declaration").click(function () {
+        $("#judge").text(judgment());
+    });
+});
